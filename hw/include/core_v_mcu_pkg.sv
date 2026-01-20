4e8bb4ae6725c259c5ced0a16813355a5202c703
Luigi Giuffrida <luigi.giuffrida.98@gmail.com>
2026-01-20 17:38:16 +0100

    Rework pgk

--- hw/include/core_v_mcu_pkg.sv
+++ hw/include/core_v_mcu_pkg.sv
@@ -1,13 +1,110 @@
+`include "axi/typedef.svh"
+`include "register_interface/typedef.svh"
+
 package core_v_mcu_pkg;
 
-  //-----------
-  // BUS Config
-  //-----------
-  localparam int unsigned NumMasters = 2; // just core and ext for now
+    //-----------
+    // BUS Config
+    //-----------
+    localparam int unsigned NumMasters = 1;
 
-  localparam int unsigned NumAxiSlaves = 1; // ext (or non for now)
-  localparam int unsigned NumRegSlaves = 2; // UART + ext only for now
-  localparam int unsigned NumSlaves = NumAxiSlaves + NumRegSlaves;
+    localparam int unsigned NumAxiSlaves = 2;
+    localparam int unsigned NumRegSlaves = 3;  // UART + ext only for now
+    localparam int unsigned NumSlaves = NumAxiSlaves + NumRegSlaves;
 
+    // AXI configuration parameters
+    localparam int unsigned AxiMstIdWidth = 4;
+    localparam int unsigned AxiSlvIdWidth = AxiMstIdWidth + $clog2(NumMasters);
+    localparam int unsigned AxiAddrWidth = 64;
+    localparam int unsigned AxiDataWidth = 64;
+    localparam int unsigned AxiUserWidth = 64;
+
+    // AXI type definitions
+
+    localparam type addr_t = logic [AxiAddrWidth-1:0];
+    localparam type axi_data_t = logic [AxiDataWidth   -1:0];
+    localparam type axi_strb_t = logic [AxiDataWidth/8 -1:0];
+    localparam type axi_user_t = logic [AxiUserWidth   -1:0];
+    localparam type axi_mst_id_t = logic [AxiMstIdWidth  -1:0];
+    localparam type axi_slv_id_t = logic [AxiSlvIdWidth  -1:0];
+
+    `AXI_TYPEDEF_ALL_CT(axi_mst, axi_mst_req_t, axi_mst_rsp_t, addr_t, axi_mst_id_t, axi_data_t,
+                        axi_strb_t, axi_user_t)
+    `AXI_TYPEDEF_ALL_CT(axi_slv, axi_slv_req_t, axi_slv_rsp_t, addr_t, axi_slv_id_t, axi_data_t,
+                        axi_strb_t, axi_user_t)
+
+    `REG_BUS_TYPEDEF_ALL(reg, addr_t, logic [31:0], logic [3:0])
+
+    typedef struct packed {
+        int unsigned idx;
+        addr_t start_addr;
+        addr_t end_addr;
+    } rule_t;
+
+    // Master indexes
+    localparam int unsigned CPU_BUS_IDX = 0;
+
+    // Slave indexes
+    localparam int unsigned MEM_BUS_IDX = 0;
+    localparam int unsigned PERIPH_BUS_IDX = 1;
+
+    // Slave addresses
+    localparam addr_t MEM_BUS_BASE_ADDR = 64'h0000_0000_0000_0000;
+    localparam addr_t MEM_BUS_END_ADDR = 64'h0000_0000_0FFF_FFFF;
+    localparam addr_t PERIPH_BUS_BASE_ADDR = 64'h1000_0000_0000_0000;
+    localparam addr_t PERIPH_BUS_END_ADDR = 64'h1000_F000_0000_0000;
+
+    // Register indexes
+    localparam int unsigned SOC_CTRL_REG_IDX = 0;
+    localparam int unsigned FAST_INTR_CTRL_REG_IDX = 1;
+    localparam int unsigned UART_REG_IDX = 2;
+
+    // Register addresses
+    localparam addr_t SOC_CTRL_REG_START_ADDR = 64'h1000_0000_0000_0000;
+    localparam addr_t SOC_CTRL_REG_SIZE = 64'h0000_1000_0000_0000;
+    localparam addr_t SOC_CTRL_REG_END_ADDR = SOC_CTRL_REG_START_ADDR + SOC_CTRL_REG_SIZE;
+    localparam addr_t FAST_INTR_CTRL_REG_START_ADDR = SOC_CTRL_REG_END_ADDR;
+    localparam addr_t FAST_INTR_CTRL_REG_SIZE = 64'h0000_1000_0000_0000;
+    localparam addr_t FAST_INTR_CTRL_REG_END_ADDR = FAST_INTR_CTRL_REG_START_ADDR + FAST_INTR_CTRL_REG_SIZE;
+    localparam addr_t UART_REG_START_ADDR = FAST_INTR_CTRL_REG_END_ADDR;
+    localparam addr_t UART_REG_SIZE = 64'h0000_1000_0000_0000;
+    localparam addr_t UART_REG_END_ADDR = UART_REG_START_ADDR + UART_REG_SIZE;
+
+    // Address mapping rules
+    localparam rule_t [NumAxiSlaves-1:0] addr_rules = '{
+        '{idx : MEM_BUS_IDX, start_addr : MEM_BUS_BASE_ADDR, end_addr : MEM_BUS_END_ADDR},
+        '{idx : PERIPH_BUS_IDX, start_addr : PERIPH_BUS_BASE_ADDR, end_addr : PERIPH_BUS_END_ADDR}
+    };
+    
+    localparam rule_t [NumRegSlaves-1:0] RegMap = '{
+        '{
+            idx : SOC_CTRL_REG_IDX,
+            start_addr : SOC_CTRL_REG_START_ADDR,
+            end_addr : SOC_CTRL_REG_END_ADDR
+        },
+        '{
+            idx : FAST_INTR_CTRL_REG_IDX,
+            start_addr : FAST_INTR_CTRL_REG_START_ADDR,
+            end_addr : FAST_INTR_CTRL_REG_END_ADDR
+        },
+        '{idx : UART_REG_IDX, start_addr : UART_REG_START_ADDR, end_addr : UART_REG_END_ADDR}
+    };
+
+    // AXI Crossbar configuration
+    localparam axi_pkg::xbar_cfg_t AxiXbarCfg = '{
+        NoSlvPorts        : NumMasters,
+        NoMstPorts        : NumAxiSlaves,
+        MaxMstTrans       : 16,
+        MaxSlvTrans       : 16,
+        FallThrough       : 1'b1,
+        LatencyMode       : 10'b0000001111,  // Low latency
+        PipelineStages    : 1,
+        AxiIdWidthSlvPorts: AxiSlvIdWidth,
+        AxiIdUsedSlvPorts : AxiSlvIdWidth,
+        UniqueIds         : 1'b1,
+        AxiAddrWidth     : AxiAddrWidth,
+        AxiDataWidth     : AxiDataWidth,
+        NoAddrRules      : NumAxiSlaves
+    };
 
 endpackage : core_v_mcu_pkg
--- hw/include/cva6_config_pkg.sv
+++ hw/include/cva6_config_pkg.sv
@@ -17,164 +17,164 @@
 
 package cva6_config_pkg;
 
-  localparam int CVA6ConfigXlen = 64;
+    localparam int CVA6ConfigXlen = 64;
 
-  localparam int CVA6ConfigRVF = 1;
-  localparam int CVA6ConfigRVD = 1;
-  localparam int CVA6ConfigF16En = 0;
-  localparam int CVA6ConfigF16AltEn = 0;
-  localparam int CVA6ConfigF8En = 0;
-  localparam int CVA6ConfigFVecEn = 0;
+    localparam int CVA6ConfigRVF = 1;
+    localparam int CVA6ConfigRVD = 1;
+    localparam int CVA6ConfigF16En = 0;
+    localparam int CVA6ConfigF16AltEn = 0;
+    localparam int CVA6ConfigF8En = 0;
+    localparam int CVA6ConfigFVecEn = 0;
 
-  localparam int CVA6ConfigCvxifEn = 1;
-  localparam int CVA6ConfigCExtEn = 1;
-  localparam int CVA6ConfigZcbExtEn = 1;
-  localparam int CVA6ConfigZcmpExtEn = 0;
-  localparam int CVA6ConfigAExtEn = 1;
-  localparam int CVA6ConfigBExtEn = 1;
-  localparam int CVA6ConfigVExtEn = 0;
-  localparam int CVA6ConfigHExtEn = 0;
-  localparam int CVA6ConfigRVZiCond = 1;
+    localparam int CVA6ConfigCvxifEn = 1;
+    localparam int CVA6ConfigCExtEn = 1;
+    localparam int CVA6ConfigZcbExtEn = 1;
+    localparam int CVA6ConfigZcmpExtEn = 0;
+    localparam int CVA6ConfigAExtEn = 1;
+    localparam int CVA6ConfigBExtEn = 1;
+    localparam int CVA6ConfigVExtEn = 0;
+    localparam int CVA6ConfigHExtEn = 0;
+    localparam int CVA6ConfigRVZiCond = 1;
 
-  localparam int CVA6ConfigAxiIdWidth = 4;
-  localparam int CVA6ConfigAxiAddrWidth = 64;
-  localparam int CVA6ConfigAxiDataWidth = 64;
-  localparam int CVA6ConfigFetchUserEn = 0;
-  localparam int CVA6ConfigFetchUserWidth = CVA6ConfigXlen;
-  localparam int CVA6ConfigDataUserEn = 0;
-  localparam int CVA6ConfigDataUserWidth = CVA6ConfigXlen;
+    localparam int CVA6ConfigAxiIdWidth = 4;
+    localparam int CVA6ConfigAxiAddrWidth = 64;
+    localparam int CVA6ConfigAxiDataWidth = 64;
+    localparam int CVA6ConfigFetchUserEn = 0;
+    localparam int CVA6ConfigFetchUserWidth = CVA6ConfigXlen;
+    localparam int CVA6ConfigDataUserEn = 0;
+    localparam int CVA6ConfigDataUserWidth = CVA6ConfigXlen;
 
-  localparam int CVA6ConfigIcacheByteSize = 16384;
-  localparam int CVA6ConfigIcacheSetAssoc = 4;
-  localparam int CVA6ConfigIcacheLineWidth = 128;
-  localparam int CVA6ConfigDcacheByteSize = 32768;
-  localparam int CVA6ConfigDcacheSetAssoc = 8;
-  localparam int CVA6ConfigDcacheLineWidth = 128;
+    localparam int CVA6ConfigIcacheByteSize = 16384;
+    localparam int CVA6ConfigIcacheSetAssoc = 4;
+    localparam int CVA6ConfigIcacheLineWidth = 128;
+    localparam int CVA6ConfigDcacheByteSize = 32768;
+    localparam int CVA6ConfigDcacheSetAssoc = 8;
+    localparam int CVA6ConfigDcacheLineWidth = 128;
 
-  localparam logic CVA6ConfigDcacheFlushOnFence = 1'b0;
-  localparam logic CVA6ConfigDcacheInvalidateOnFlush = 1'b0;
+    localparam logic CVA6ConfigDcacheFlushOnFence = 1'b0;
+    localparam logic CVA6ConfigDcacheInvalidateOnFlush = 1'b0;
 
-  localparam int CVA6ConfigDcacheIdWidth = 3;
-  localparam int CVA6ConfigMemTidWidth = CVA6ConfigAxiIdWidth;
+    localparam int CVA6ConfigDcacheIdWidth = 3;
+    localparam int CVA6ConfigMemTidWidth = CVA6ConfigAxiIdWidth;
 
-  localparam int CVA6ConfigWtDcacheWbufDepth = 8;
+    localparam int CVA6ConfigWtDcacheWbufDepth = 8;
 
-  localparam int CVA6ConfigNrScoreboardEntries = 8;
+    localparam int CVA6ConfigNrScoreboardEntries = 8;
 
-  localparam int CVA6ConfigNrLoadPipeRegs = 1;
-  localparam int CVA6ConfigNrStorePipeRegs = 0;
-  localparam int CVA6ConfigNrLoadBufEntries = 8;
+    localparam int CVA6ConfigNrLoadPipeRegs = 1;
+    localparam int CVA6ConfigNrStorePipeRegs = 0;
+    localparam int CVA6ConfigNrLoadBufEntries = 8;
 
-  localparam int CVA6ConfigRASDepth = 2;
-  localparam int CVA6ConfigBTBEntries = 32;
-  localparam int CVA6ConfigBHTEntries = 128;
+    localparam int CVA6ConfigRASDepth = 2;
+    localparam int CVA6ConfigBTBEntries = 32;
+    localparam int CVA6ConfigBHTEntries = 128;
 
-  localparam int CVA6ConfigTvalEn = 1;
+    localparam int CVA6ConfigTvalEn = 1;
 
-  localparam int CVA6ConfigNrPMPEntries = 8;
+    localparam int CVA6ConfigNrPMPEntries = 8;
 
-  localparam int CVA6ConfigPerfCounterEn = 1;
+    localparam int CVA6ConfigPerfCounterEn = 1;
 
-  localparam config_pkg::cache_type_t CVA6ConfigDcacheType = config_pkg::WT;
+    localparam config_pkg::cache_type_t CVA6ConfigDcacheType = config_pkg::WT;
 
-  localparam int CVA6ConfigMmuPresent = 1;
+    localparam int CVA6ConfigMmuPresent = 1;
 
-  localparam int CVA6ConfigRvfiTrace = 1;
+    localparam int CVA6ConfigRvfiTrace = 1;
 
-  localparam config_pkg::cva6_user_cfg_t cva6_cfg = '{
-      XLEN: unsigned'(CVA6ConfigXlen),
-      VLEN: unsigned'(64),
-      FpgaEn: bit'(0),  // for Xilinx and Altera
-      FpgaAlteraEn: bit'(0),  // for Altera (only)
-      TechnoCut: bit'(0),
-      SuperscalarEn: bit'(0),
-      ALUBypass: bit'(0),
-      NrCommitPorts: unsigned'(2),
-      AxiAddrWidth: unsigned'(CVA6ConfigAxiAddrWidth),
-      AxiDataWidth: unsigned'(CVA6ConfigAxiDataWidth),
-      AxiIdWidth: unsigned'(CVA6ConfigAxiIdWidth),
-      AxiUserWidth: unsigned'(CVA6ConfigDataUserWidth),
-      MemTidWidth: unsigned'(CVA6ConfigMemTidWidth),
-      NrLoadBufEntries: unsigned'(CVA6ConfigNrLoadBufEntries),
-      RVF: bit'(CVA6ConfigRVF),
-      RVD: bit'(CVA6ConfigRVD),
-      XF16: bit'(CVA6ConfigF16En),
-      XF16ALT: bit'(CVA6ConfigF16AltEn),
-      XF8: bit'(CVA6ConfigF8En),
-      RVA: bit'(CVA6ConfigAExtEn),
-      RVB: bit'(CVA6ConfigBExtEn),
-      ZKN: bit'(1),
-      RVV: bit'(CVA6ConfigVExtEn),
-      RVC: bit'(CVA6ConfigCExtEn),
-      RVH: bit'(CVA6ConfigHExtEn),
-      RVZCB: bit'(CVA6ConfigZcbExtEn),
-      RVZCMP: bit'(CVA6ConfigZcmpExtEn),
-      RVZCMT: bit'(0),
-      XFVec: bit'(CVA6ConfigFVecEn),
-      CvxifEn: bit'(CVA6ConfigCvxifEn),
-      CoproType: config_pkg::COPRO_NONE,
-      RVZiCond: bit'(CVA6ConfigRVZiCond),
-      RVZicntr: bit'(1),
-      RVZihpm: bit'(1),
-      NrScoreboardEntries: unsigned'(CVA6ConfigNrScoreboardEntries),
-      PerfCounterEn: bit'(CVA6ConfigPerfCounterEn),
-      MmuPresent: bit'(CVA6ConfigMmuPresent),
-      RVS: bit'(1),
-      RVU: bit'(1),
-      SoftwareInterruptEn: bit'(1),
-      HaltAddress: 64'h800,
-      ExceptionAddress: 64'h808,
-      RASDepth: unsigned'(CVA6ConfigRASDepth),
-      BTBEntries: unsigned'(CVA6ConfigBTBEntries),
-      BPType: config_pkg::BHT,
-      BHTEntries: unsigned'(CVA6ConfigBHTEntries),
-      BHTHist: unsigned'(3),
-      DmBaseAddress: 64'h0,
-      TvalEn: bit'(CVA6ConfigTvalEn),
-      DirectVecOnly: bit'(0),
-      NrPMPEntries: unsigned'(CVA6ConfigNrPMPEntries),
-      PMPCfgRstVal: {64{64'h0}},
-      PMPAddrRstVal: {64{64'h0}},
-      PMPEntryReadOnly: 64'd0,
-      PMPNapotEn: bit'(1),
-      NOCType: config_pkg::NOC_TYPE_AXI4_ATOP,
-      NrNonIdempotentRules: unsigned'(2),
-      NonIdempotentAddrBase: 1024'({64'b0, 64'b0}),
-      NonIdempotentLength: 1024'({64'b0, 64'b0}),
-      NrExecuteRegionRules: unsigned'(3),
-      ExecuteRegionAddrBase: 1024'({64'h8000_0000, 64'h1_0000, 64'h0}),
-      ExecuteRegionLength: 1024'({64'h40000000, 64'h10000, 64'h1000}),
-      NrCachedRegionRules: unsigned'(1),
-      CachedRegionAddrBase: 1024'({64'h8000_0000}),
-      CachedRegionLength: 1024'({64'h40000000}),
-      MaxOutstandingStores: unsigned'(7),
-      DebugEn: bit'(1),
-      SDTRIG: bit'(0),
-      Mcontrol6: bit'(0),
-      Icount: bit'(0),
-      Etrigger: bit'(0),
-      Itrigger: bit'(0),
-      AxiBurstWriteEn: bit'(0),
-      IcacheByteSize: unsigned'(CVA6ConfigIcacheByteSize),
-      IcacheSetAssoc: unsigned'(CVA6ConfigIcacheSetAssoc),
-      IcacheLineWidth: unsigned'(CVA6ConfigIcacheLineWidth),
-      DCacheType: CVA6ConfigDcacheType,
-      DcacheByteSize: unsigned'(CVA6ConfigDcacheByteSize),
-      DcacheSetAssoc: unsigned'(CVA6ConfigDcacheSetAssoc),
-      DcacheLineWidth: unsigned'(CVA6ConfigDcacheLineWidth),
-      DcacheFlushOnFence: bit'(CVA6ConfigDcacheFlushOnFence),
-      DcacheInvalidateOnFlush: bit'(CVA6ConfigDcacheInvalidateOnFlush),
-      DataUserEn: unsigned'(CVA6ConfigDataUserEn),
-      WtDcacheWbufDepth: int'(CVA6ConfigWtDcacheWbufDepth),
-      FetchUserWidth: unsigned'(CVA6ConfigFetchUserWidth),
-      FetchUserEn: unsigned'(CVA6ConfigFetchUserEn),
-      InstrTlbEntries: int'(16),
-      DataTlbEntries: int'(16),
-      UseSharedTlb: bit'(0),
-      SharedTlbDepth: int'(64),
-      NrLoadPipeRegs: int'(CVA6ConfigNrLoadPipeRegs),
-      NrStorePipeRegs: int'(CVA6ConfigNrStorePipeRegs),
-      DcacheIdWidth: int'(CVA6ConfigDcacheIdWidth)
-  };
+    localparam config_pkg::cva6_user_cfg_t cva6_cfg = '{
+        XLEN: unsigned'(CVA6ConfigXlen),
+        VLEN: unsigned'(64),
+        FpgaEn: bit'(0),  // for Xilinx and Altera
+        FpgaAlteraEn: bit'(0),  // for Altera (only)
+        TechnoCut: bit'(0),
+        SuperscalarEn: bit'(0),
+        ALUBypass: bit'(0),
+        NrCommitPorts: unsigned'(2),
+        AxiAddrWidth: unsigned'(CVA6ConfigAxiAddrWidth),
+        AxiDataWidth: unsigned'(CVA6ConfigAxiDataWidth),
+        AxiIdWidth: unsigned'(CVA6ConfigAxiIdWidth),
+        AxiUserWidth: unsigned'(CVA6ConfigDataUserWidth),
+        MemTidWidth: unsigned'(CVA6ConfigMemTidWidth),
+        NrLoadBufEntries: unsigned'(CVA6ConfigNrLoadBufEntries),
+        RVF: bit'(CVA6ConfigRVF),
+        RVD: bit'(CVA6ConfigRVD),
+        XF16: bit'(CVA6ConfigF16En),
+        XF16ALT: bit'(CVA6ConfigF16AltEn),
+        XF8: bit'(CVA6ConfigF8En),
+        RVA: bit'(CVA6ConfigAExtEn),
+        RVB: bit'(CVA6ConfigBExtEn),
+        ZKN: bit'(1),
+        RVV: bit'(CVA6ConfigVExtEn),
+        RVC: bit'(CVA6ConfigCExtEn),
+        RVH: bit'(CVA6ConfigHExtEn),
+        RVZCB: bit'(CVA6ConfigZcbExtEn),
+        RVZCMP: bit'(CVA6ConfigZcmpExtEn),
+        RVZCMT: bit'(0),
+        XFVec: bit'(CVA6ConfigFVecEn),
+        CvxifEn: bit'(CVA6ConfigCvxifEn),
+        CoproType: config_pkg::COPRO_NONE,
+        RVZiCond: bit'(CVA6ConfigRVZiCond),
+        RVZicntr: bit'(1),
+        RVZihpm: bit'(1),
+        NrScoreboardEntries: unsigned'(CVA6ConfigNrScoreboardEntries),
+        PerfCounterEn: bit'(CVA6ConfigPerfCounterEn),
+        MmuPresent: bit'(CVA6ConfigMmuPresent),
+        RVS: bit'(1),
+        RVU: bit'(1),
+        SoftwareInterruptEn: bit'(1),
+        HaltAddress: 64'h800,
+        ExceptionAddress: 64'h808,
+        RASDepth: unsigned'(CVA6ConfigRASDepth),
+        BTBEntries: unsigned'(CVA6ConfigBTBEntries),
+        BPType: config_pkg::BHT,
+        BHTEntries: unsigned'(CVA6ConfigBHTEntries),
+        BHTHist: unsigned'(3),
+        DmBaseAddress: 64'h0,
+        TvalEn: bit'(CVA6ConfigTvalEn),
+        DirectVecOnly: bit'(0),
+        NrPMPEntries: unsigned'(CVA6ConfigNrPMPEntries),
+        PMPCfgRstVal: {64{64'h0}},
+        PMPAddrRstVal: {64{64'h0}},
+        PMPEntryReadOnly: 64'd0,
+        PMPNapotEn: bit'(1),
+        NOCType: config_pkg::NOC_TYPE_AXI4_ATOP,
+        NrNonIdempotentRules: unsigned'(2),
+        NonIdempotentAddrBase: 1024'({64'b0, 64'b0}),
+        NonIdempotentLength: 1024'({64'b0, 64'b0}),
+        NrExecuteRegionRules: unsigned'(3),
+        ExecuteRegionAddrBase: 1024'({64'h8000_0000, 64'h1_0000, 64'h0}),
+        ExecuteRegionLength: 1024'({64'h40000000, 64'h10000, 64'h1000}),
+        NrCachedRegionRules: unsigned'(1),
+        CachedRegionAddrBase: 1024'({64'h8000_0000}),
+        CachedRegionLength: 1024'({64'h40000000}),
+        MaxOutstandingStores: unsigned'(7),
+        DebugEn: bit'(1),
+        SDTRIG: bit'(0),
+        Mcontrol6: bit'(0),
+        Icount: bit'(0),
+        Etrigger: bit'(0),
+        Itrigger: bit'(0),
+        AxiBurstWriteEn: bit'(0),
+        IcacheByteSize: unsigned'(CVA6ConfigIcacheByteSize),
+        IcacheSetAssoc: unsigned'(CVA6ConfigIcacheSetAssoc),
+        IcacheLineWidth: unsigned'(CVA6ConfigIcacheLineWidth),
+        DCacheType: CVA6ConfigDcacheType,
+        DcacheByteSize: unsigned'(CVA6ConfigDcacheByteSize),
+        DcacheSetAssoc: unsigned'(CVA6ConfigDcacheSetAssoc),
+        DcacheLineWidth: unsigned'(CVA6ConfigDcacheLineWidth),
+        DcacheFlushOnFence: bit'(CVA6ConfigDcacheFlushOnFence),
+        DcacheInvalidateOnFlush: bit'(CVA6ConfigDcacheInvalidateOnFlush),
+        DataUserEn: unsigned'(CVA6ConfigDataUserEn),
+        WtDcacheWbufDepth: int'(CVA6ConfigWtDcacheWbufDepth),
+        FetchUserWidth: unsigned'(CVA6ConfigFetchUserWidth),
+        FetchUserEn: unsigned'(CVA6ConfigFetchUserEn),
+        InstrTlbEntries: int'(16),
+        DataTlbEntries: int'(16),
+        UseSharedTlb: bit'(0),
+        SharedTlbDepth: int'(64),
+        NrLoadPipeRegs: int'(CVA6ConfigNrLoadPipeRegs),
+        NrStorePipeRegs: int'(CVA6ConfigNrStorePipeRegs),
+        DcacheIdWidth: int'(CVA6ConfigDcacheIdWidth)
+    };
 
 endpackage
